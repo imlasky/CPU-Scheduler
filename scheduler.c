97766e407dca53c192453d97cd0b3d28d3c186a5
imlasky <ian.lasky1@gmail.com>
2017-02-05 14:34:50 -0500

    Added sjf and rr functionality

--- scheduler.c
+++ scheduler.c
@@ -1,10 +1,14 @@
-//
+// 
 //
 //  Implementation of First-Come First-Served
 //  preemptive Shortest Job First, and Round-Robin
 //  CPU scheduling algorithms
 //
+//  Group 18
 //
+//  Jacob Garman
+//	Ian Lasky
+//	Sean Reedy
 //
 //
 
@@ -28,10 +32,10 @@ int arrivalTimeMin(int [], int [], int);
 
 int main()
 {
-	char fileName[20];
-	printf("Please enter the file name: ");
-	scanf("%s", fileName);
-	Data data = readIn(fileName);
+//	char fileName[20];
+//	printf("Please enter the file name: ");
+//	scanf("%s", fileName);
+	Data data = readIn("process.in");
 
 	switch (data.scheduleType[0]) 
 	{
@@ -59,21 +63,27 @@ void fcfs(Data data)
 
 	fileOut = fopen("processes.out","w");
 	
+	//Print out first couple lines
 	fprintf(fileOut,"%d processes\n", data.processCount);
-	fprintf(fileOut,"Using First-Come First-Served\n\n");	
+	fprintf(fileOut,"Using First Come First Served\n\n");	
 
+	//Declare and initialize variables
 	int minimum = 10000;
 	int minimumIndex = 1;
 	int minimumIndex_new = 0;
 	int time= 0;
 	int i;
 
+	//Tracker has two values for each process: wait time = time selected - arrival time
+	//And turnaround time = time completed - arrival time 
 	int tracker[data.processCount][2];
 	int processCounter = data.processCount;
 
 	
+	//Continue while the time is still within the runFor parameter
 	while (time <= data.runFor) 
 	{
+		//Cycle through the processes and see if they have arrived
 		for (i = 0; i < data.processCount; i++) 
 		{
 			if (data.arrival[i] == time) 
@@ -81,16 +91,23 @@ void fcfs(Data data)
 				fprintf(fileOut,"Time %d: %s arrived\n",time,data.processName[i]);
 			}
 		}
+		//Use helper function to determine the lowest arrival time
 		minimumIndex_new = arrivalTimeMin(data.arrival, data.burst, data.processCount);
+		//If it has changed, then change which process should be selected
 		if(minimumIndex_new != minimumIndex)
 		{
 			fprintf(fileOut,"Time %d: %s selected (burst %d)\n", time, data.processName[minimumIndex_new], data.burst[minimumIndex_new]);
 			minimumIndex = minimumIndex_new;
+			//New process selected, so edit wait time tracking parameter
 			tracker[minimumIndex][0] = time-data.arrival[minimumIndex];
 		}
 
+		//Decrement this process's burst by one and increment the time by one
 		data.burst[minimumIndex]--;
 		time ++;
+		//If this process's burst has completed print out that it has finished,
+		//Edit the turn around time tracking parameter, and decrement the available
+		//Process counter
 		if (data.burst[minimumIndex] == 0) 
 		{
 			fprintf(fileOut,"Time %d: %s finished\n",time,data.processName[minimumIndex]);
@@ -98,6 +115,8 @@ void fcfs(Data data)
 			processCounter--;
 		}
 
+		//If all processes have completed, print out all wait times and turn around times
+		//For the processes
 		if (processCounter == 0) 
 		{
 			fprintf(fileOut,"Finished at time %d\n\n", time);
@@ -110,6 +129,13 @@ void fcfs(Data data)
 			break;
 		}
 		
+	}
+	for (i = 0; i < data.processCount; i++) 
+	{
+		if (data.burst[i] != 0) 
+		{
+			fprintf(fileOut, "%s did not finish\n", data.processName[i]);
+		}
 	}
 	fclose(fileOut);
 	
@@ -133,12 +159,180 @@ int arrivalTimeMin(int arrival[], int burst[], int processCount)
 
 void sjf(Data data)
 {
+	FILE *fileOut;
+
+	fileOut = fopen("processes.out","w");
 	
+	fprintf(fileOut,"%d processes\n", data.processCount);
+	fprintf(fileOut,"Using Shortest Job First (Pre)\n\n");	
+
+	int i; 
+	int minTime;
+	int minIndex;
+	int currentJob=-1; 
+	int remainingJobs = data.processCount; 
+	int time=0; 
+	int wait[data.processCount];
+	int turnaround[data.processCount]; 
+	int burstTime[data.processCount]; // records original burst time for each process
+
+	for(i=0; i<data.processCount; i++)
+	{
+		burstTime[i] = data.burst[i];
+	 }
+	
+	while (time < data.runFor)
+	{
+
+	       minTime=1000; // time of shortest job 
+	       minIndex=-1; // index of shortest job  
+		for(i=0; i<data.processCount; i++)
+		{
+		    if ( data.arrival[i] == time ) // find arriving processes 
+		    {
+			fprintf(fileOut,"Time %d: %s arrived\n", time, data.processName[i]);
+		    }
+		    if (data.burst[i] <= minTime && data.burst[i]>0 && data.arrival[i] <= time)  // find shortest process that hasn't finished and has arrived   
+		    {
+			minIndex = i; 
+			minTime = data.burst[i]; 
+		    }
+		}
+		
+		if (minIndex == -1) // no process can run -> IDLE 
+		{
+		    fprintf(fileOut,"Time %d: IDLE\n", time); 
+		    time++;
+		}
+		else
+		{
+			if (minIndex != currentJob) // if new process selected 
+			{
+				fprintf(fileOut,"Time %d: %s selected (burst %d)\n", time, data.processName[minIndex], data.burst[minIndex]);
+		    	}
+			currentJob = minIndex; 
+			data.burst[currentJob]--; 
+			time++; 
+			if ( data.burst[currentJob] == 0 ) // job finished 
+			{
+			    fprintf(fileOut,"Time %d: %s finished\n", time, data.processName[currentJob]); 
+			    wait[currentJob] = time - (data.arrival[currentJob] + burstTime[currentJob]);
+			    turnaround[currentJob] = time - data.arrival[currentJob]; 
+			    remainingJobs--; 
+			}
+		}
+		
+
+	}
+
+	if ( remainingJobs == 0 ) // success 
+	{
+	    fprintf(fileOut,"Finished at time %d\n\n", time);
+	}
+	else
+	{
+	    fprintf(fileOut, "Did not finish (time %d)\n\n", time);
+	}
+
+	for(i=0; i<data.processCount; i++) // output wait and turnaround times, or state process did not finish 
+	{
+	    if ( data.burst[i] == 0 ) 
+		fprintf(fileOut,"%s wait %d turnaround %d\n", data.processName[i], wait[i], turnaround[i]);
+	    else 
+		fprintf(fileOut, "%s did not finish\n", data.processName[i]); 
+	}
+
+	fclose(fileOut); 
 }
 
 void rr(Data data)
 {
+	FILE *fileOut;
+
+	fileOut = fopen("processes.out","w");
 	
+	fprintf(fileOut,"%d processes\n", data.processCount);
+	fprintf(fileOut,"Using Round-Robin\n");	
+	fprintf(fileOut,"Quantum %d\n\n", data.quantum);
+
+	int ready[1024];
+	int qhead=0, qtail=0;
+	int time= 0;
+	int active = -1;
+	int remaining = 0;
+	int i;
+
+	int finished[50];
+	//need to preserve the original duration, because wait is end_time - start_time - run_time
+	int duration[50];
+	for(i=0; i<data.processCount; i++)
+		duration[i] = data.burst[i];
+	int processCounter = data.processCount;
+
+	
+	while (time <= data.runFor) 
+	{
+		//check if any processes arrive this tick
+		for (i = 0; i < data.processCount; i++) 
+		{
+			if (data.arrival[i] == time) 
+			{
+				fprintf(fileOut,"Time %d: %s arrived\n",time,data.processName[i]);
+				ready[qtail++] = i;
+			}
+		}
+		
+		//Context switch
+		if(remaining <= 0) {
+			//if not idle			
+			if(active >= 0) {
+				ready[qtail++] = active;
+				active = ready[qhead++];
+				remaining = data.quantum;
+				fprintf(fileOut,"Time %d: %s selected (burst %d)\n", time, data.processName[active], data.burst[active]);
+			} else {
+				if(qtail != qhead) {
+					active = ready[qhead++];
+					remaining = data.quantum;
+					fprintf(fileOut,"Time %d: %s selected (burst %d)\n", time, data.processName[active], data.burst[active]);
+				} else {
+					fprintf(fileOut, "Time %d: IDLE\n", time);
+				}
+			}
+		}
+		
+		//simulation update
+		data.burst[active]--;
+		time++;
+		remaining--;
+		
+		//wrap up a completed process
+		if (data.burst[active] == 0) 
+		{
+			fprintf(fileOut,"Time %d: %s finished\n",time,data.processName[active]);
+			finished[active] = time;
+			active = -1;
+			processCounter--;
+			remaining = 0;
+		}
+		
+		
+
+		if (time == data.runFor) 
+		{
+			fprintf(fileOut,"Finished at time %d\n\n", time);
+			for (i = 0; i < data.processCount; i++) 
+			{
+				if(data.burst[i] == 0)//process finished
+					fprintf(fileOut,"%s wait %d turnaround %d\n",data.processName[i],finished[i] - data.arrival[i] - duration[i],finished[i] - data.arrival[i]);
+				else//process did not finish
+					fprintf(fileOut,"%s did not finish.\n",data.processName[i]);
+			}
+			break;
+		}
+		
+	}
+	fclose(fileOut);
 }
 
 
